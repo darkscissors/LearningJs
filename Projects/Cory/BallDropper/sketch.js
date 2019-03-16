1a991cd8b8e1f7cdfcdf19bba395372473efb86d
darkscissors <darkscissors00@gmail.com>
2019-03-15 21:46:28 -0700

    added a line drawing thingy for some reason

--- Projects/Cory/BallDropper/sketch.js
+++ Projects/Cory/BallDropper/sketch.js
@@ -2,6 +2,10 @@
 let balls = [];
 let mouseHeld = false;
 let startX, startY;
+let clickMode = true; // true == ball drops
+let lines = [];
+let colliders = [];
+
 function setup()
 {
   createCanvas(1000,1000);
@@ -15,6 +19,11 @@ function draw()
   fill(255);
   textSize(30);
   text('Press space to clear all balls\nClick and drag to shoot a ball in a given dirrection\nMore distance dragged = more speed',15,30);
+  if(clickMode)
+  {
+    fill(52);
+  }
+  ellipse(900,50,50,50);
 
   for(let i = 0; i < balls.length; i++)
   {
@@ -22,8 +31,17 @@ function draw()
     balls[i].show();
     balls[i].step();
   }
+  for(let i = 0; i < colliders.length; i++)
+  {
+    ellipse(colliders[i].x,colliders[i].y,8,8);
+  }
   if(mouseHeld)
   line(startX,startY,mouseX,mouseY);
+let oldX,oldY;
+  for(let i = 0; i < lines.length; i++)
+  {
+    lines[i].show();
+  }
 
 }
 
@@ -31,26 +49,38 @@ function keyPressed()
 {
 
   if(key == ' ')
+  {
+    lines = [];
     balls = [];
+  }
+  if(key == 'A')
+    clickMode = !clickMode;
 
 }
 
 function mousePressed()
 {
+  mouseHeld = true;
 
-
-mouseHeld = true;
-startX = mouseX;
-startY = mouseY;
-
+    startX = mouseX;
+    startY = mouseY;
 }
 
 function mouseReleased()
 {
-  let div = 10;
-  let vX_ = ((startX - mouseX) / div)*-1;
-  let vY_ = ((startY - mouseY) / div)*-1;
-spawnBall(startX,startY,vX_,vY_);
+  if(clickMode)
+  {
+    let div = 10;
+    let vX_ = ((startX - mouseX) / div)*-1;
+    let vY_ = ((startY - mouseY) / div)*-1;
+    spawnBall(startX,startY,vX_,vY_);
+  }
+  else
+  {
+    lines.push(new Line(startX,startY,mouseX,mouseY));
+
+
+  }
 
 mouseHeld = false;
 
@@ -89,28 +119,49 @@ class Ball
 
   step()
   {
-    this.y = this.y + this.vY;
-    this.vY = this.vY + 1;
-    this.x = this.x + this.vX;
-    if(this.y >= 1000 - 10)
-    {
-      this.vY = this.vY * -1;
+
+      this.y = this.y + this.vY;
       this.vY = this.vY + 1;
+      this.x = this.x + this.vX;
+      if(this.y >= 1000 - 10)
+      {
+        this.vY = this.vY * -1;
+        this.vY = this.vY + 1;
 
-    }
-    if(this.x >= 1000 - 8 || this.x <= 0 + 8)
-    {
-      this.vX = this.vX * -1;
-      this.color = color(random(0,255),random(0,255),random(0,255));
+      }
+      if(this.x >= 1000 - 8 || this.x <= 0 + 8)
+      {
+        this.vX = this.vX * -1;
+        this.color = color(random(0,255),random(0,255),random(0,255));
 
+      }
+      if(this.y > 1000)
+      {
+        this.y = 1000;
+      }
     }
-    if(this.y > 1000)
-    {
-      this.y = 1000;
-    }
+}
 
+class Line
+{
+  constructor(x1,y1,x2,y2)
+  {
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+    this.len = dist(x1,y1,x2,y2);
+  }
+
+  show()
+  {
+    fill(82);
+    line(this.x1,this.y1,this.x2,this.y2);
+    //ellipse(this._x,this._y,this.r,this.r);
 
   }
 
 
-}
+
+
+  }
